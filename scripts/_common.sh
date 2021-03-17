e465e9403e1486a4451c06e8d3fa8dc1cd4360fd
ericgaspar <junk.eg@free.fr>
2021-03-17 09:45:28 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -39,17 +39,18 @@ myynh_install() {
 	
         ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-local==3.2.1
 	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-MusicBox-Webclient==3.1.0
-	ynh_script_progression --message="Installing Mopidy-YouTube..." --weight=1
+	ynh_script_progression --message="Installing plugins..." --weight=20
+	#ynh_script_progression --message="Installing Mopidy-YouTube..." --weight=1
 	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-YouTube==3.2
-	ynh_script_progression --message="Installing Mopidy-YTMusic..." --weight=1
+	#ynh_script_progression --message="Installing Mopidy-YTMusic..." --weight=1
 	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-YTMusic==0.2.2
-	ynh_script_progression --message="Installing Mopidy-RadioNet..." --weight=1
+	#ynh_script_progression --message="Installing Mopidy-RadioNet..." --weight=1
 	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-RadioNet==0.2.2
-	ynh_script_progression --message="Installing Mopidy-Podcast..." --weight=1
+	#ynh_script_progression --message="Installing Mopidy-Podcast..." --weight=1
 	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-Podcast==3.0.0
-	ynh_script_progression --message="Installing Mopidy-Podcast-iTunes..." --weight=1
+	#ynh_script_progression --message="Installing Mopidy-Podcast-iTunes..." --weight=1
 	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-Podcast-iTunes==3.0.0
-	ynh_script_progression --message="Installing Mopidy-SoundCloud..." --weight=1
+	#ynh_script_progression --message="Installing Mopidy-SoundCloud..." --weight=1
 	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-SoundCloud
 #	ynh_script_progression --message="Installing Mopidy-Spotify..." --weight=1
 #	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-Spotify   Spotify Lib currently not available!!
