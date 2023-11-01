d6b96ceee11be4c435dd6a6122b48b989ae8ef0e
Fazarel <35377747+Fazarel@users.noreply.github.com>
2023-11-01 11:28:56 +0100

    Update _common.sh
    Trying to solve dependencies and pygobject install

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@
 pkg_dependencies="acl build-essential python3-dev python3-pip python3-venv git postgresql postgresql-contrib \
 gir1.2-gst-plugins-base-1.0 gir1.2-gstreamer-1.0 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly lsb-base \
 python3-gst-1.0 python3-tornado debconf python3-pkg-resources python3-pykka python3-requests \
-gstreamer1.0-alsa gstreamer1.0-pulseaudio gstreamer1.0-tools"
+gstreamer1.0-alsa gstreamer1.0-pulseaudio gstreamer1.0-tools libcairo2-dev libffi-dev libgirepository1.0-dev libglib2.0-dev"
 
 #=================================================
 # PERSONAL HELPERS
@@ -28,20 +28,22 @@ myynh_install() {
 	ynh_exec_as $app $final_path/env/bin/pip install --upgrade --no-cache-dir pip
 
 	# to make Gstreamer visible in Python environment
+ 	$final_path/env/bin/python3 -m pip install wheel
 	$final_path/env/bin/python3 -m pip install vext
 	$final_path/env/bin/python3 -m pip install --no-binary=:all: vext.gi
+ 	$final_path/env/bin/python3 -m pip install --ignore-installed --no-cache pygobject
 
 	# install essential packages
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy==$(ynh_app_upstream_version)
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-local==3.2.1
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-MusicBox-Webclient==3.1.0
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-YouTube==3.3
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir mopidy-ytmusic==0.3.0
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-RadioNet==0.2.2
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-Podcast==3.0.0
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-Podcast-iTunes==3.0.0
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-SoundCloud==3.0.1
-	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-MPD==3.1.0
+	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy
+	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-local
+ 	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-MPD
+	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-MusicBox-Webclient
+#	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-YouTube
+#	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-YTMusic
+#	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-RadioNet
+#	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-Podcast
+#	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-Podcast-iTunes
+#	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy-SoundCloud
 	)
 }
 
