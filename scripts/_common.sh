18bf58023d3d6b5adf6b3c30be1ea34b4b397a46
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-10-21 10:03:40 +0200

    fix permissions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,15 +11,12 @@ cmd_file="/usr/local/bin/mopidyctl"
 media_dir="/home/yunohost.multimedia/share/Music"
 
 _mopidy_install() {
-    python3 -m venv --upgrade "$install_dir/venv"
-    venvpy="$install_dir/venv/bin/python3"
-
-    ynh_exec_as_app "$venvpy" -m pip install --upgrade --no-cache-dir pip
-
-    ynh_exec_as_app "$venvpy" -m pip install PyGObject
+    ynh_exec_as_app python3 -m venv "$install_dir/venv"
+    ynh_hide_warnings ynh_exec_as_app "$install_dir/venv/bin/pip" install --upgrade --no-cache-dir pip
+    ynh_hide_warnings ynh_exec_as_app "$install_dir/venv/bin/pip" install PyGObject
 
     # install essential packages
-    ynh_exec_as_app "$venvpy" -m pip install --no-cache-dir \
+    ynh_hide_warnings ynh_exec_as_app "$install_dir/venv/bin/pip" install --no-cache-dir \
         Mopidy=="$(ynh_app_upstream_version)" \
         Mopidy-local==3.2.1 \
         Mopidy-MusicBox-Webclient==3.1.0 \
